bb76d720796b59ccc47412c010cfb82d07f14506
NotSoHealthy <aminbenhamouda16@gmail.com>
2025-03-24 00:36:12 +0100

    layout finished

---
+++ src/Form/CongeType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Conge;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CongeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('request_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('start_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('end_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('reason')
+            ->add('status')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Conge::class,
+        ]);
+    }
+}
