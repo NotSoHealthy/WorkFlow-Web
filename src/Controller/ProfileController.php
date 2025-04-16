49191ce8b80093652225564f0a7bcbd3cff522a5
NotSoHealthy <aminbenhamouda16@gmail.com>
2025-04-16 20:35:31 +0100

    Check verification

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -17,25 +17,37 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProfileController extends AbstractController
 {
     #[Route(path: '/edit-profile', name: 'app_profile_edit_profile')]
-    public function editProfile(DelegationService $delegationService,Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function editProfile(DelegationService $delegationService, Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         /** @var User $user */
         $user = $this->getUser();
+        $originalUser = clone $user;
+
         $form = $this->createForm(ProfileType::class, $user);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $plainPassword = $form->get('password')->getData();
-
             if (!empty($plainPassword)) {
                 $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
             }
+
             $entityManager->persist($user);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_profile_edit_profile');
+        } else {
+            // Restore original values to avoid keeping invalid data in memory
+            $user->setFirstName($originalUser->getFirstName());
+            $user->setLastName($originalUser->getLastName());
+            $user->setEmail($originalUser->getEmail());
+            $user->setNumber($originalUser->getNumber());
+            $user->setGouvernorat($originalUser->getGouvernorat());
+            $user->setAddress($originalUser->getAddress());
+            $user->setImageUrl($originalUser->getImageUrl());
         }
-            
-        
+
+
         return $this->render('profile/edit_profile.html.twig', ["form" => $form->createView(), "gouvernorats" => $delegationService->getDelegations()]);
     }
 }
