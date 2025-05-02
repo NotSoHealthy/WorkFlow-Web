b81bb7e64bd372bf754a70b4c5d90b22783b1d72
zakariabouchaddakh <zakariazikou513@gmail.com>
2025-05-02 12:10:03 +0100

    ...

--- src/Form/DepartmentType.php
+++ src/Form/DepartmentType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Department;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,28 +18,28 @@ class DepartmentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('Name', TextType::class, [
-            'label' => 'Nom',
-            'attr' => ['placeholder' => 'Entrez le Nom du Département'],
-            'required' => true,
-            'empty_data' => '', // this is what fixes the error!
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Le nom du département est requis.',
-                ]),
-            ],
-        ])
-        ->add('Year_Budget', NumberType::class, [
-            'label' => 'Budget Annuel',
-            'required' => true,
-            'empty_data' => '0', 
-            'attr' => ['placeholder' => 'Entrez le Budget Annuel'],
-            'constraints' => [
-                new NotBlank([
-                    'message' => 'Le budget annuel est requis.',
-                ]),
-            ],
-        ])
+            ->add('Name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Entrez le Nom du Département'],
+                'required' => true,
+                'empty_data' => '',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom du département est requis.',
+                    ]),
+                ],
+            ])
+            ->add('Year_Budget', NumberType::class, [
+                'label' => 'Budget Annuel',
+                'required' => true,
+                'empty_data' => '0',
+                'attr' => ['placeholder' => 'Entrez le Budget Annuel'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le budget annuel est requis.',
+                    ]),
+                ],
+            ])
             ->add('manager', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'first_name',
